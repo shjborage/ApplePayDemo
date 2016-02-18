820f9c7121700c5bcbac8c49293987a749d29e0f
Eric <eric@saick.net>
2016-02-18 11:01:13 +0800

    Add project and Capabilities add `ApplePay`

---
+++ ApplePayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplePayDemo
+//
+//  Created by shihaijie on 2/18/16.
+//  Copyright © 2016 Saick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
