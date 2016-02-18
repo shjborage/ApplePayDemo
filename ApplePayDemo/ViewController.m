2c19c1b9672eeb423a6d48c6f6a33fe38040f245
Eric <eric@saick.net>
2016-02-18 11:47:22 +0800

    Add docs、resources、guides and setup button code.

--- ApplePayDemo/ViewController.m
+++ ApplePayDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+@import PassKit;
 
 @interface ViewController ()
 
@@ -17,11 +18,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 是否支持ApplePay，系统版本、硬件、ParentControl
+    BOOL isSupportPay = [PKPaymentAuthorizationViewController canMakePayments];
+    NSLog(@"isSupportPay: %d", isSupportPay);
+    if (!isSupportPay) {
+        return;
+    } else {
+        // continue
+    }
+    
+    // 是否支持这些支持方式（可能没有绑卡）
+    NSArray *networks = @[PKPaymentNetworkPrivateLabel, PKPaymentNetworkVisa, PKPaymentNetworkMasterCard];
+    BOOL canPay = [PKPaymentAuthorizationViewController canMakePaymentsUsingNetworks:networks];
+    NSLog(@"canPay: %d", canPay);
+    
+    if (!canPay) {
+        // setup
+        PKPaymentButton *setupButton = [PKPaymentButton buttonWithType:PKPaymentButtonTypeSetUp style:PKPaymentButtonStyleBlack];
+        [setupButton addTarget:self action:@selector(applePaySetupButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:setupButton];
+        
+        setupButton.center = CGPointMake(self.view.frame.size.width/2, 100.0f);
+    } else {
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (void)applePaySetupButtonPressed:(PKPaymentButton *)button
+{
+    // openPaymentSetup
+    [[PKPassLibrary new] openPaymentSetup];
 }
 
 @end
--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 ApplePay调研及Demo
 
 ##Ref
++   苹果官方主页 <https://developer.apple.com/apple-pay/>
++   苹果官方资源 <https://developer.apple.com/apple-pay/Apple-Pay-Identity-Guidelines.pdf>
++   苹果官方开发指南 <https://developer.apple.com/library/ios/ApplePay_Guide/CreateRequest.html#//apple_ref/doc/uid/TP40014764-CH3-SW2>
++   苹果官方图片资源下载 <https://developer.apple.com/devcenter/download.action?path=/ios/apple_pay_resources/Apple_Pay_Resources.zip>
 +   Apple Pay 开发教程：创造更好的支付体验 <http://www.cocoachina.com/cms/wap.php?action=article&id=10888>
++   苹果官方Apple Pay开发文档（中文版）：开始使用Apple Pay <http://www.cocoachina.com/cms/wap.php?action=article&id=9573>
